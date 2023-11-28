bc869d69d00b1572722fba2a5a6b241343b07c9e
gukazma <gukazma@proton.me>
2023-11-28 11:55:59 +0800

    ✨ feat: cona cmake

--- conanfile.py
+++ conanfile.py
@@ -5,49 +5,20 @@ from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 class CppTemplateProjectRecipe(ConanFile):
     name = "CppTemplateProject"
     version = "1.0"
-    package_type = "library"
-
-    # Optional metadata
-    license = "<Put the package license here>"
-    author = "<Put your name here> <And your email here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of hello package here>"
-    topics = ("<Put some tag here>", "<here>", "<and here>")
-
-    # Binary configuration
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
-    
-    # Sources are located in the same place as this recipe, copy them to the recipe
-    exports_sources = "CMakeLists.txt", "Modules/*", "Modules/*"
+    generators = "cmake", "cmake_find_package_multi", "cmake_paths"
 
-    def config_options(self):
-        if self.settings.os == "Windows":
-            self.options.rm_safe("fPIC")
+    exports_sources = "CMakeLists.txt", "Modules/*", "Modules/*"
     def requirements(self):
         self.requires("gtest/1.14.0")
         self.requires("zlib/1.3")
         self.options["zlib"].shared = True
-        
     def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
-
-    def layout(self):
-        cmake_layout(self)
-
-    def generate(self):
-        deps = CMakeDeps(self)
-        deps.generate()
-        tc = CMakeToolchain(self)
-        tc.generate()
-
+        del self.settings.compiler.cppstd
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
-
     def package(self):
         cmake = CMake(self)
         cmake.install()
@@ -56,4 +27,4 @@ class CppTemplateProjectRecipe(ConanFile):
         self.cpp_info.libs = ["CppTemplateProject"]
 
     def imports(self):
-        self.copy("*", "./build/bin", "bin")
+        self.copy("*.dll", "./bin", "bin")
